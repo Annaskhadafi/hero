15eb72674a2ebe5acd54a582070d2936565086a6
Annaskhadafi <mochamad.khadafi@chitraparatama.co.id>
2026-06-07 09:10:40 +0800

    chore: remove bis_skin_checked script, purely browser extension issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata, Viewport } from "next";
 import { Geist_Mono, Inter, Manrope } from "next/font/google";
-import Script from "next/script";
 import { PwaRegistration } from "@/components/pwa-registration";
 import { ThemeProvider } from "@/components/theme-provider";
 import "./globals.css";
@@ -52,13 +51,6 @@ export const viewport: Viewport = {
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="id" suppressHydrationWarning>
-      <head>
-        <Script id="clean-bis-skin" strategy="beforeInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `(function(){var r=new MutationObserver(function(){document.querySelectorAll("[bis_skin_checked]").forEach(function(e){e.removeAttribute("bis_skin_checked")})});r.observe(document.documentElement,{attributes:true,childList:true,subtree:true});setTimeout(function(){r.disconnect()},3e3)})()`
-          }}
-        />
-      </head>
       <body
         className={`${inter.variable} ${manrope.variable} ${geistMono.variable} antialiased`}
         suppressHydrationWarning
